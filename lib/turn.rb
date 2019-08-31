9d8725c6e0a24691cb2adeb76ee8830fb8666a7d
Edgar V <evenegas9@ucmerced.edu>
2019-08-31 03:30:46 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -2,7 +2,6 @@ def turn
 end
 
 def display_board board
-  # " X | X | X "
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
