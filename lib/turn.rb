f43bff8284cbca841adb282c98c68213497c9ed0
Edgar V <evenegas9@ucmerced.edu>
2019-08-31 03:21:30 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -14,8 +14,12 @@ def valid_move? board, index
 
 end
 
-def position_taken?
-
+def position_taken? board, index
+  if board[index] == " " || board[index] == "" || board[index].nil?
+    false
+  else
+    true
+  end
 end
 
 def move board, index, token = 'X'
